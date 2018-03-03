29967daa723627b7eb2d446f0ff48d0ddf270646
karelyshv <karelyshv12@gmail.com>
2018-03-03 18:42:58 -0400

    Update and rename main.c to LecturaUnCanal.c

--- Comunicacion_serial/codewarrior/LecturaUnCanal.c
+++ Comunicacion_serial/codewarrior/LecturaUnCanal.c
@@ -1,4 +1,4 @@
-/* #/* ###################################################################
+/* ###################################################################
 **     Filename    : main.c
 **     Project     : ADC
 **     Processor   : MC9S08QE128CLK
@@ -33,9 +33,9 @@
 #include "AS1.h"
 #include "AD1.h"
 #include "Bit1.h"
+#include "Bit2.h"
 #include "TI1.h"
 #include "Cap1.h"
-#include "Bit2.h"
 /* Include shared modules, which are used for whole project */
 #include "PE_Types.h"
 #include "PE_Error.h"
@@ -45,21 +45,17 @@
 /* User includes (#include below this line is not maintained by Processor Expert) */
 
 unsigned char estado = ESPERAR;
-unsigned char CodError;
-unsigned int Enviados = 3;    // Esta variable no aporta nada más sino el número de elementos del arreglo a enviar.
+unsigned int Enviados = 2;    //Número de elementos del arreglo a enviar
 
 typedef union{
-unsigned char u8[3];
+unsigned char u8[2];
 unsigned int u16;
 }AMPLITUD;
 
-volatile AMPLITUD iADC, Senal;
+volatile AMPLITUD iADC;
 
-unsigned int Marcador = 0xF1;
-unsigned int D1 = 0;
-unsigned int D2 = 0;
-unsigned int A = 0;
-unsigned int B = 0;
+unsigned char Senal[3]={0xF1,0x00,0x00};  
+bool D1, D2;
 
 void main(void)
 {
@@ -82,42 +78,29 @@ void main(void)
         break;
         
       case MEDIR:
-        CodError = AD1_Measure(TRUE);
-        CodError = AD1_GetValue16(&iADC.u16);
-        if ( Bit1_GetVal() == 0){
-        	D1 = 1;
-        }
-        if (Bit2_GetVal() == 0){
-        	D2 = 1;
-        }
-        
+        AD1_Measure(TRUE);
+        AD1_GetValue16(&iADC.u16);		//Lee señal analógica
+        D1 = Bit1_GetVal();				//Lee señal digital 1
+		D2 = Bit2_GetVal();				//Lee señal digiral 2
         estado = ENVIAR;
         break;
     
       case ENVIAR:
     	  
-   		B = iADC.u16 & 0x7F;
-    	A = (iADC.u16 >> 7) & 0x1F;
-     	if(D1 == 1)
+   		Senal[2] = (iADC.u16 >> 4) & 0x7F;   			// Selecciono 0A7A6A5A4A3A2A1 , 7 bits de señal analógica menos significativos
+   		Senal[1] = (iADC.u16 >> 11) & 0x1F;		// Selecciono 000A12A11A10A9A8 , 5 bits señal analógica más significativos
+     	if(D1 == 0)
    		{
-   			A = A | 0x40; // or con 01000000
-   			D1 = 0;
+     		Senal[1] = Senal[1] | 0x40; 		// or con 01000000 = 0[1]D2A12A11A10A9A8
    		}
-   		if(D2 == 1)
+   		if(D2 == 0)
     	{
-    		A = A | 0x20; // or con 00100000
-    		D2 = 0;
+   			Senal[1] = Senal[1] | 0x20; 		// or con 00100000 = 0D1[1]A12A11A10A9A8
    		}
-
-   	    Senal.u8[0] = Marcador;
-   	 	Senal.u8[1] = A;
-  	 	Senal.u8[2] = B;
-      
-      
-      // ENVIAR SOLO LA MEDICIÓN DE 16 BITS SIN TRAMA NI PROTOCOLO:
-        
-        CodError = AS1_SendBlock(Senal.u8,3,&Enviados); //El arreglo con la medición está en iADC.u8 (notar que es un apuntador)
-        estado = ESPERAR;
+    
+        AS1_SendBlock(Senal,3,&Enviados); 		//Se envia por serial arreglo de tres bytes con el protocolo aplicado
+  	 	        
+  	 	estado = ESPERAR;
         
         break;
         
