49b056b1cf924759555116d8cc12c14259735d29
karelyshv <karelyshv12@gmail.com>
2018-04-13 20:16:17 -0400

    Update LecturaDosCanales.c

--- Comunicacion_serial/codewarrior/LecturaDosCanales.c
+++ Comunicacion_serial/codewarrior/LecturaDosCanales.c
@@ -32,12 +32,10 @@
 #include "Events.h"
 #include "AS1.h"
 #include "AD1.h"
-#include "Bit1.h"
-#include "Bit2.h"
-#include "Bit3.h"
-#include "Bit4.h"
 #include "TI1.h"
 #include "Cap1.h"
+#include "Bits1.h"
+#include "Bits2.h"
 /* Include shared modules, which are used for whole project */
 #include "PE_Types.h"
 #include "PE_Error.h"
@@ -47,7 +45,7 @@
 /* User includes (#include below this line is not maintained by Processor Expert) */
 
 unsigned char estado = ESPERAR;
-unsigned int Enviados = 2 , i, A;    //Número de elementos del arreglo a enviar
+unsigned int i, Enviados = 2;    //Número de elementos del arreglo a enviar
 
 typedef union{
 unsigned char u8[2];
@@ -56,7 +54,8 @@ unsigned int u16[3];
 
 volatile AMPLITUD iADC;
 
-unsigned char Senal[5]={0xF1,0x00,0x00,0x00,0x00}, D[4]={0x00,0x00,0x00,0x00};  
+unsigned char Senal[5]={0xF1,0x00,0x00,0x00,0x00} ,  D[4]={0x00,0x00,0x00,0x00};
+bool D1, D2;
 
 void main(void)
 {
@@ -79,39 +78,39 @@ void main(void)
         break;
         
       case MEDIR:
-        AD1_MeasureChan(TRUE,0);
-        AD1_MeasureChan(TRUE,1);
-        AD1_GetChanValue16(0,&iADC.u16[0]);		//Lee señal analógica(0,&iADC.u16);
-        AD1_GetChanValue16(1,&iADC.u16[2]);
-
-        D[0] = Bit1_GetVal();				//Lee señal digital 1
-  		  D[1] = Bit2_GetVal();				//Lee señal digiral 2
-        D[2] = Bit3_GetVal();				//Lee señal digital 3
-  		  D[3] = Bit4_GetVal();   		//Lee señal digiral 4
-		
-        estado = ENVIAR;
-        break;
-    
-      case ENVIAR:
-    	  
+    	AD1_MeasureChan(TRUE,0);
+    	AD1_GetChanValue16(0,&iADC.u16[0]);
+    	AD1_MeasureChan(TRUE,1);
+    	AD1_GetChanValue16(1,&iADC.u16[2]);   	
+        //AD1_Measure(TRUE);
+        //AD1_GetValue16(&iADC.u16);		//Lee señal analógica
+    	D[0] = 	Bits1_GetBit(0);			//Lee señal digital 1
+    	D[1] = 	Bits1_GetBit(1);			//Lee señal digital 2
+        D[2] = 	Bits2_GetBit(0);			//Lee señal digital 3
+        D[3] =  Bits2_GetBit(1); 			//Lee señal digital 4
+    			
+    	estado = ENVIAR;
+    	break;
+    	    
+    	case ENVIAR:
+    	   	  
     	for (i=1;i<=3;i=i+2){  
-    		
-   		Senal[i+1] = (iADC.u16[i-1] >> 4) & 0x7F;   			// Selecciono 0A7A6A5A4A3A2A1 , 7 bits de señal analógica menos significativos
-   		Senal[i] = (iADC.u16[i-1] >> 11) & 0x1F;		// Selecciono 000A12A11A10A9A8 , 5 bits señal analógica más significativos
-    
-   		if(D[i-1] == 0)
-   		{
-     		Senal[i] = Senal[i] | 0x40; 		// or con 01000000 = 0[1]D2A12A11A10A9A8
-   		}
-   		if(D[i] == 0)
-    	{
-   			Senal[i] = Senal[i] | 0x20; 		// or con 00100000 = 0D1[1]A12A11A10A9A8
-   		}
-    
+    	    		
+    		Senal[i+1] = (iADC.u16[i-1] >> 4) & 0x7F;   	// Selecciono 0A7A6A5A4A3A2A1 , 7 bits de señal analógica menos significativos
+    	   	Senal[i] = (iADC.u16[i-1] >> 11) & 0x1F;		// Selecciono 000A12A11A10A9A8 , 5 bits señal analógica más significativos
+    	    if(D[i-1] == 0)
+    	   	{
+    	    	Senal[i] = Senal[i] | 0x40; 		// or con 01000000 = 0[1]D2A12A11A10A9A8
+    	   	}
+    	   	if(D[i] == 0)
+    	    {
+    	   		Senal[i] = Senal[i] | 0x20; 		// or con 00100000 = 0D1[1]A12A11A10A9A8
+    	   	}
     	}  
+    	    	
     	AS1_SendBlock(Senal,5,&Enviados); 		//Se envia por serial arreglo de cinco bytes con el protocolo aplicado
-    	        
-  	 	estado = ESPERAR;
+    	    	        
+    	estado = ESPERAR;
         
         break;
         
